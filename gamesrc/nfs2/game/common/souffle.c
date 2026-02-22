792677282e9727cbad02aba8fcfbeb2d0dd26d27
Thiekus <13563072+thiekus@users.noreply.github.com>
2026-02-22 18:25:45 +0700

    Redefines souffle.c and stats.c

--- gamesrc/nfs2/game/common/souffle.c
+++ gamesrc/nfs2/game/common/souffle.c
@@ -3,16 +3,121 @@
 #include <game/common/souffle.h>
 
 // GLOBAL: 0003:000289e0 _gWindDir 005119e0 (bss)
+// LOCAL : 0000: MODULE_386 (type = 9)
+// int gWindDir;
+
 // GLOBAL: 0003:000289e4 _gCISouffle 005119e4 (bss)
+// LOCAL : 0011: MODULE_386 (type = 9)
+// int gCISouffle;
+
 // GLOBAL: 0003:000289e8 _gISouffle 005119e8 (bss)
+// LOCAL : 0024: MODULE_386 (type = 61)
+// DRender_tISouffle gISouffle[60];
+
 // GLOBAL: 0003:00029b7c _gTMoveSouffle 00512b7c (bss)
+// LOCAL : 0036: MODULE_386 (type = 9)
+// int gTMoveSouffle;
+
+// FAKE TYPES: delete them after correctly implemented!
+typedef struct DRender_tISouffle{int fakeValue;} DRender_tISouffle;
+typedef struct coorddef{int fakeValue;} LIBCOORD;
+typedef struct DRender_tView{int fakeValue;} DRender_tView;
 
 // GLOBAL: 0001:000449c6 GetGustWind_ 004459c6 (code)
+// LOCAL : 0054: NEAR_RTN_386 (return type = 63)
+// ARG 0 : 0076: LOCAL (type = 62) (EAX)
+GAME_FUNC void DEFAULT_CALL GetGustWind(DRender_tISouffle* is)
+{
+    CALL_WATCALL_VOID_1(0x004459c6,
+        DRender_tISouffle* is,
+        is
+    );
+}
+
 // GLOBAL: 0001:00044a1e GetAngleWind_ 00445a1e (code)
+// LOCAL : 007D: NEAR_RTN_386 (return type = 63)
+// ARG 0 : 00A0: LOCAL (type = 62) (EAX)
+GAME_FUNC void DEFAULT_CALL GetAngleWind(DRender_tISouffle* is)
+{
+    CALL_WATCALL_VOID_1(0x00445a1e,
+        DRender_tISouffle* is,
+        is
+    );
+}
+
 // GLOBAL: 0001:00044a58 MoveAngleWind_ 00445a58 (code)
+// LOCAL : 00A7: NEAR_RTN_386 (return type = 63)
+// ARG 0 : 00CB: LOCAL (type = 62) (EAX)
+GAME_FUNC void DEFAULT_CALL MoveAngleWind(DRender_tISouffle* is)
+{
+    // 00D2: BLOCK_386
+    // int i; // 00DE: LOCAL (type = 9)
+    // int maxc; // 00E4: LOCAL (type = 9)
+    // 00ED: BLOCK_386
+    // LIBCOORD vempty; // 00F9: LOCAL (type = 46)
+    // DRender_tISouffle* is; // 0104: LOCAL (type = 62)
+    CALL_WATCALL_VOID_1(0x00445a58,
+        DRender_tISouffle* is,
+        is
+    );
+}
+
 // GLOBAL: 0001:00044a9e Souffle_Add_ 00445a9e (code)
+// LOCAL : 010B: NEAR_RTN_386 (return type = 65)
+// ARG 0 : 0132: LOCAL (type = 64) (EAX)
+// ARG 1 : 0140: LOCAL (type = 9) (EDX)
+// ARG 2 : 0149: LOCAL (type = 64) (EBX)
+GAME_FUNC DRender_tISouffle* DEFAULT_CALL Souffle_Add(LIBCOORD* soufflept, int type, LIBCOORD* vec)
+{
+    // int inserti; // 0151: LOCAL (type = 9)
+    // 015D: BLOCK_386
+    // DRender_tISouffle* is; // 0169: LOCAL (type = 62)
+    CALL_WATCALL_RESULT_3(DRender_tISouffle*, 0x00445a9e,
+        LIBCOORD* soufflept, int type, LIBCOORD* vec,
+        soufflept, type, vec
+    );
+}
+
 // GLOBAL: 0001:00044c24 Souffle_DoSouffle_ 00445c24 (code)
+// LOCAL : 0170: NEAR_RTN_386 (return type = 21)
+GAME_FUNC void DEFAULT_CALL Souffle_DoSouffle(void)
+{
+    // LIBCOORD w; // 0196: LOCAL (type = 46)
+    // int i; // 019C: LOCAL (type = 9)
+    // 01A2: BLOCK_386
+    // DRender_tISouffle* is; // 01AE: LOCAL (type = 62)
+    CALL_WATCALL_VOID_0(0x00445c24);
+}
+
 // GLOBAL: 0001:00044dec Souffle_InsertFacet_ 00445dec (code)
+// LOCAL : 01B5: NEAR_RTN_386 (return type = 77)
+// ARG 0 : 01DF: LOCAL (type = 76) (EAX)
+GAME_FUNC void DEFAULT_CALL Souffle_InsertFacet(DRender_tView* Vi)
+{
+    // int i; // 01E6: LOCAL (type = 9)
+    CALL_WATCALL_VOID_1(0x00445dec,
+        DRender_tView* Vi,
+        Vi
+    );
+}
+
 // GLOBAL: 0001:00044e91 Souffle_InitTrackSouffle_ 00445e91 (code)
+// LOCAL : 01EC: NEAR_RTN_386 (return type = 21)
+GAME_FUNC void DEFAULT_CALL Souffle_InitTrackSouffle(void)
+{
+    CALL_WATCALL_VOID_0(0x00445e91);
+}
+
 // GLOBAL: 0001:00044ec4 Souffle_RestartTrackSouffle_ 00445ec4 (code)
+// LOCAL : 0219: NEAR_RTN_386 (return type = 21)
+GAME_FUNC void DEFAULT_CALL Souffle_RestartTrackSouffle(void)
+{
+    CALL_WATCALL_VOID_0(0x00445ec4);
+}
+
 // GLOBAL: 0001:00044ee3 Souffle_KillTrackSouffle_ 00445ee3 (code)
+// LOCAL : 0249: NEAR_RTN_386 (return type = 21)
+GAME_FUNC void DEFAULT_CALL Souffle_KillTrackSouffle(void)
+{
+    CALL_WATCALL_VOID_0(0x00445ee3);
+}
--- gamesrc/nfs2/game/common/stats.c
+++ gamesrc/nfs2/game/common/stats.c
@@ -3,12 +3,110 @@
 #include <game/common/stats.h>
 
 // GLOBAL: 0003:0002a188 _Stats_racePosition 00513188 (bss)
+// LOCAL : 0000: MODULE_386 (type = 63)
+// Stats_tPosition Stats_racePosition[12];
+
+// FAKE TYPES: delete them after correctly implemented!
+typedef struct Car_tObj{int fakeValue;} Car_tObj;
 
 // GLOBAL: 0001:00045366 Stats_DoPlayerGlue_ 00446366 (code)
+// LOCAL : 0023: NEAR_RTN_386 (return type 23)
+GAME_FUNC void DEFAULT_CALL Stats_DoPlayerGlue(void)
+{
+    // int i; // 004A: LOCAL (type = 9)
+    // int dist; // 0050: LOCAL (type = 9)
+    // int humanLeader; // 0059: LOCAL (type = 9)
+    CALL_WATCALL_VOID_0(0x00446366);
+}
+
 // GLOBAL: 0001:0004556a Stats_ClearPosition_ 0044656a (code)
+// LOCAL : 0069: NEAR_RTN_386 (return type = 23)
+GAME_FUNC void DEFAULT_CALL Stats_ClearPosition(void)
+{
+    // int i; // 0091: LOCAL (type = 9)
+    CALL_WATCALL_VOID_0(0x0044656a);
+}
+
 // GLOBAL: 0001:000455da Stats_GetPosition_ 004465da (code)
+// LOCAL : 0097: NEAR_RTN_386 (return type = 186)
+// ARG 0 : 00C1: LOCAL (type = 185) (EAX)
+GAME_FUNC int DEFAULT_CALL Stats_GetPosition(Car_tObj* carObj)
+{
+    // int i; // 00CD: LOCAL (type = 9)
+    // int position; // 00D3: LOCAL (type = 9)
+    // int carindex; // 00E0: LOCAL (type = 9)
+    CALL_WATCALL_RESULT_1(int, 0x004465da,
+        Car_tObj* carObj,
+        carObj
+    );
+}
+
 // GLOBAL: 0001:00045646 Stats_GetNumOpponents_ 00446646 (code)
+// LOCAL : 00ED: NEAR_RTN_386 (return type = 187)
+GAME_FUNC int DEFAULT_CALL Stats_GetNumOpponents(void)
+{
+    // int i; // 0119: LOCAL (type = 9)
+    // int numCars; // 011F: LOCAL (type = 9)
+    CALL_WATCALL_RESULT_0(int, 0x00446646);
+}
+
 // GLOBAL: 0001:0004569d Stats_GetNextCar_ 0044669d (code)
+// LOCAL : 012B: NEAR_RTN_386 (return type = 188)
+// ARG 0 : 0154: LOCAL (type = 9) (EAX)
+GAME_FUNC Car_tObj* DEFAULT_CALL Stats_GetNextCar(int position)
+{
+    // 0161: BLOCK_386
+    // int r0; // 016D: LOCAL (type = 9)
+    // int r1; // 0174: LOCAL (type = 9)
+    // int r2; // 017B: LOCAL (type = 9)
+    // int r3; // 0182: LOCAL (type = 9)
+    // int r4; // 0189: LOCAL (type = 9)
+    // 0190: BLOCK_386
+    // int r1; // 019C: LOCAL (type = 9)
+    // int r2; // 01A3: LOCAL
+    // int r3; // 01AA: LOCAL
+    // int r4; // 01B1: LOCAL
+    // 01B8: BLOCK_386
+    // int j; // 01C4: LOCAL
+    // 01CA: BLOCK_386
+    // int i; // 01D6: LOCAL
+    CALL_WATCALL_RESULT_1(Car_tObj*, 0x0044669d,
+        int position,
+        position
+    );
+}
+
 // GLOBAL: 0001:000456e1 Stats_TrackStats_ 004466e1 (code)
+// LOCAL : 01DC: NEAR_RTN_386 (return type = 189)
+// ARG 0 : 0204: LOCAL (type = 185) (EAX)
+GAME_FUNC void DEFAULT_CALL Stats_TrackStats(Car_tObj* carObj)
+{
+    CALL_WATCALL_VOID_1(0x004466e1,
+        Car_tObj* carObj,
+        carObj
+    );
+}
+
 // GLOBAL: 0001:00045b37 Stats_ExtrapolateOpponentTimes_ 00446b37 (code)
+// LOCAL : 0210: NEAR_RTN_386 (return type = 190)
+// ARG 0 : 0246: LOCAL (type = 9)
+GAME_FUNC void DEFAULT_CALL Stats_ExtrapolateOpponentTimes(int type)
+{
+    // int i; // 024F: LOCAL (type = 9)
+    // int extrapolatedTime; // 0255: LOCAL (type = 9)
+    // int bAllHumansHaveFinished; // 026A: LOCAL (type = 9)
+    CALL_WATCALL_VOID_1(0x00446b37,
+        int type,
+        type
+    );
+}
+
 // GLOBAL: 0001:00045e78 Stats_TrackEndGame_ 00446e78 (code)
+// LOCAL : 0285: NEAR_RTN_386 (return type = 23)
+GAME_FUNC void DEFAULT_CALL Stats_TrackEndGame(void)
+{
+    // int i; // 02AC: LOCAL (type = 9)
+    // int numKnockOutPlayers; // 02B2: LOCAL (type = 9)
+    // int Stats_PlayersFinishedRace; // 02C9: LOCAL (type = 9)
+    CALL_WATCALL_VOID_0(0x00446e78);
+}
