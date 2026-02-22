e173a63c125bb3c945790f9b639a270d8ee8ab36
Thiekus <13563072+thiekus@users.noreply.github.com>
2026-02-22 07:13:21 +0700

    Redefines sim.c and quatern.c

--- gamesrc/nfs2/game/common/quatern.c
+++ gamesrc/nfs2/game/common/quatern.c
@@ -2,5 +2,51 @@
 #include <renfs2.h>
 #include <game/common/quatern.h>
 
+// FAKE TYPES: delete them after correctly implemented!
+typedef struct tQuat{int fakeValue;} Quatern_quat;
+typedef struct coorddef{int fakeValue;} LIBCOORD;
+typedef struct matrixtdef{int fakeValue;} MATRIX3DT;
+
 // GLOBAL: 0001:0003a5d1 Quatern_Interpolate_ 0043b5d1 (code)
+// LOCAL : 0008: NEAR_RTN_386 (return type = 47)
+// ARG 0 : 0038: LOCAL (type = 42) (EAX)
+// ARG 1 : 003F: LOCAL (type = 42) (EDX)
+// ARG 2 : 0046: LOCAL (type = 46) (EBX)
+// ARG 3 : 004E: LOCAL (type = 46) (ECX)
+// ARG 4 : 0056: LOCAL (type = 9)
+// ARG 5 : 0061: LOCAL (type = 42)
+// ARG 6 : 0067: LOCAL (type = 42)
+GAME_FUNC void DEFAULT_CALL Quatern_Interpolate(Quatern_quat* q0, Quatern_quat* q1, LIBCOORD* cp0, LIBCOORD* cp1, int weight, Quatern_quat* q, LIBCOORD* cp)
+{
+    // int one_weight; // 006E: LOCAL (type = 9)
+    CALL_WATCALL_VOID_7(0x0043b5d1,
+        Quatern_quat* q0, Quatern_quat* q1, LIBCOORD* cp0, LIBCOORD* cp1,
+        int weight, Quatern_quat* q, LIBCOORD* cp,
+        q0, q1, cp0, cp1, weight, q, cp
+    );
+}
+
 // GLOBAL: 0001:0003a75d Quatern_QuatToMat_ 0043b75d (code)
+// LOCAL : 007D: NEAR_RTN_386 (return type = 53)
+// ARG 0 : 00A7: LOCAL (type = 42)
+// ARG 1 : 00AD: LOCAL (type = 52)
+GAME_FUNC void DEFAULT_CALL Quatern_QuatToMat(Quatern_quat* q, MATRIX3DT* matrix)
+{
+    // int one; // 00B8: LOCAL (type = 9)
+    // int x; // 00C0: LOCAL (type = 9)
+    // int y; // 00C6: LOCAL (type = 9)
+    // int z; // 00CC: LOCAL (type = 9)
+    // int wx; // 00D2: LOCAL (type = 9)
+    // int wy; // 00D9: LOCAL (type = 9)
+    // int wz; // 00E0: LOCAL (type = 9)
+    // int xx; // 00E7: LOCAL (type = 9)
+    // int xy; // 00EE: LOCAL (type = 9)
+    // int xz; // 00F5: LOCAL (type = 9)
+    // int yy; // 00FC: LOCAL (type = 9)
+    // int yz; // 0103: LOCAL (type = 9)
+    // int zz; // 010A: LOCAL (type = 9)
+    CALL_WATCALL_VOID_2(0x0043b75d,
+        Quatern_quat* q, MATRIX3DT* matrix,
+        q, matrix
+    );
+}
--- gamesrc/nfs2/game/common/sim.c
+++ gamesrc/nfs2/game/common/sim.c
@@ -3,19 +3,101 @@
 #include <game/common/sim.h>
 
 // GLOBAL: 0002:0000eef8 _InBetween 004dbef8 (data)
+// LOCAL : 001F: MODULE_386 (type = 9)
+// int InBetween;
+
 // GLOBAL: 0002:0000eefc _simpause 004dbefc (data)
+// LOCAL : 0044: MODULE_386 (type = 9)
+// int simpause;
+
 // GLOBAL: 0002:0000ef00 _picture 004dbf00 (data)
+// LOCAL : 000F: MODULE_386 (type = 9)
+// int picture;
 
 // GLOBAL: 0003:00027514 _simVar 00510514 (bss)
+// LOCAL : 0000: MODULE_386 (type = 64)
+// Sim_tSimSystemVar simVar;
+
 // GLOBAL: 0003:0002752c _skipRender 0051052c (bss)
+// LOCAL : 0031: MODULE_386 (type = 9)
+// int skipRender;
+
 // GLOBAL: 0003:00027530 _unPauseDelay 00510530 (bss)
+// LOCAL : 0055: MODULE_386 (type = 9)
+// int unPauseDelay;
+
 // GLOBAL: 0003:00027534 _simGlobal 00510534 (bss)
+// LOCAL : 006A: MODULE_386 (type = 76)
+// Sim_tSimGlobalVar simGlobal;
 
 // GLOBAL: 0001:000425e6 Sim_CaptureScreen_ 004435e6 (code)
+// LOCAL : 0084: NEAR_RTN_386 (return type = 23)
+GAME_FUNC void DEFAULT_CALL Sim_CaptureScreen(void)
+{
+    // char string[80]; // 00AA: LOCAL (type = 77)
+    CALL_WATCALL_VOID_0(0x004435e6);
+}
+
 // GLOBAL: 0001:0004263a Sim_StartUp_ 0044363a (code)
+// LOCAL : 00B5: NEAR_RTN_386 (return type = 23)
+GAME_FUNC void DEFAULT_CALL Sim_StartUp(void)
+{
+    CALL_WATCALL_VOID_0(0x0044363a);
+}
+
 // GLOBAL: 0001:0004273e Sim_Restart_ 0044373e (code)
+// LOCAL : 00D5: NEAR_RTN_386 (return type = 23)
+GAME_FUNC void DEFAULT_CALL Sim_Restart(void)
+{
+    CALL_WATCALL_VOID_0(0x0044373e);
+}
+
 // GLOBAL: 0001:000427d0 Sim_CleanUp_ 004437d0 (code)
+// LOCAL : 00F5: NEAR_RTN_386 (return type = 23)
+GAME_FUNC void DEFAULT_CALL Sim_CleanUp(void)
+{
+    CALL_WATCALL_VOID_0(0x004437d0);
+}
+
 // GLOBAL: 0001:00042837 Sim_ProcessSimSchedules_ 00443837 (code)
+// LOCAL : 0115: NEAR_RTN_386 (return type = 78)
+// ARG 0 : 0143: LOCAL (type = 9) (EAX)
+GAME_FUNC void DEFAULT_CALL Sim_ProcessSimSchedules(int time)
+{
+    CALL_WATCALL_VOID_1(0x00443837,
+        int time,
+        time
+    );
+}
+
 // GLOBAL: 0001:00042a0c Sim_ProcessPause_ 00443a0c (static pubdef) (code)
+// LOCAL : 014C: NEAR_RTN_386 (return type = 23)
+GAME_FUNC void DEFAULT_CALL Sim_ProcessPause(void)
+{
+    // int r; // 0171: LOCAL (type = 9)
+    CALL_WATCALL_VOID_0(0x00443a0c);
+}
+
 // GLOBAL: 0001:00042ae3 Sim_CheckForPause_ 00443ae3 (code)
+// LOCAL : 0177: NEAR_RTN_386 (return type = 79)
+// ARG 0 : 01A0: LOCAL (type = 9) (EAX)
+GAME_FUNC int DEFAULT_CALL Sim_CheckForPause(int checkInput)
+{
+    // 01AF: BLOCK_386
+    // int realTick; // 01BB: LOCAL (type = 9)
+    // int diffReal; // 01C8: LOCAL (type = 9)
+    // int diffGoal; // 01D5: LOCAL (type = 9)
+    CALL_WATCALL_RESULT_1(int, 0x00443ae3,
+        int checkInput,
+        checkInput
+    );
+}
+
 // GLOBAL: 0001:00042bb1 Sim_MainGameLoop_ 00443bb1 (code)
+// LOCAL : 01E2: NEAR_RTN_386 (return type = 23)
+GAME_FUNC void DEFAULT_CALL Sim_MainGameLoop(void)
+{
+    // int lastRealTick; // 0207: LOCAL (type = 9)
+    // int lastGoalTick; // 0218: LOCAL (type = 9)
+    CALL_WATCALL_VOID_0(0x00443bb1);
+}
