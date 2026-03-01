e825b65d8188d18f3c484521fde49a01355d2381
Thiekus <13563072+thiekus@users.noreply.github.com>
2026-03-01 09:46:22 +0700

    Redefines mpause.c and part of eacfile.c

--- gamesrc/nfs2/game/common/eacfile.c
+++ gamesrc/nfs2/game/common/eacfile.c
@@ -3,33 +3,339 @@
 #include <game/common/eacfile.h>
 
 // GLOBAL - 0002:0000ef28 _filemutex 004dbf28 (static pubdef) (data)
+// LOCAL  - 0094: MODULE_386 (type = 9)
+// GLOBAL: NFSW 0x004dbf28
+// int filemutex;
+
 // GLOBAL - 0002:0000ef2c _maxwinfiles 004dbf2c (static pubdef) (data)
+// LOCAL  - 002A: MODULE_386 (type = 9)
+// GLOBAL: NFSW 0x004dbf2c
+// int maxwinfiles;
+
 // GLOBAL - 0002:0000ef30 _winfiles 004dbf30 (static pubdef) (data)
+// LOCAL  - 00D0: MODULE_386 (type = 349)
+// GLOBAL: NFSW 0x004dbf30
+// struct WIN32_FILE* winfiles;
+
 // GLOBAL - 0002:0000ef34 _mirrorhandle 004dbf34 (static pubdef) (data)
+// LOCAL  - 00A6: MODULE_386 (type = 9)
+// GLOBAL: NFSW 0x004dbf34
+// int mirrorhandle;
+
 // GLOBAL - 0002:0000ef38 _mirrorreadahead 004dbf38 (static pubdef) (data)
+// LOCAL  - 00E2: MODULE_386 (type = 9)
+// GLOBAL: NFSW 0x004dbf38
+// int mirrorreadahead;
+
 // GLOBAL - 0002:0000ef3c _mirrorcount 004dbf3c (static pubdef) (data)
+// LOCAL  - 006A: MODULE_386 (type = 9)
+// GLOBAL: NFSW 0x004dbf3c
+// int mirrorcount;
+
 // GLOBAL - 0002:0000ef40 _mirrorsize 004dbf40 (static pubdef) (data)
+// LOCAL  - 0057: MODULE_386 (type = 9)
+// GLOBAL: NFSW 0x004dbf40
+// int mirrorsize;
+
 // GLOBAL - 0002:0000ef44 _mirrorbaseptr 004dbf44 (static pubdef) (data)
+// LOCAL  - 0014: MODULE_386 (type = 13)
+// GLOBAL: NFSW 0x004dbf44
+// char* mirrorbaseptr;
+
 // GLOBAL - 0002:0000ef48 _mirrorblockflags 004dbf48 (static pubdef) (data)
+// LOCAL  - 003E: MODULE_386 (type = 48)
+// GLOBAL: NFSW 0x004dbf48
+// long* mirrorblockflags;
+
 // GLOBAL - 0003:0002a444 _drivemutex 00513444 (static pubdef) (bss)
+// LOCAL  - 0000: MODULE_386 (type = 343)
+// GLOBAL: NFSW 0x00513444
+// int drivemutex[32];
+
 // GLOBAL - 0003:0002a4c4 _mirrorthread 005134c4 (static pubdef) (bss)
+// LOCAL  - 007E: MODULE_386 (type = 346)
+// GLOBAL: NFSW 0x005134c4
+// THREAD mirrorthread;
+
 // GLOBAL - 0003:0002a4d8 _mirrorsignal 005134d8 (static pubdef) (bss)
+// LOCAL  - 00BB: MODULE_386 (type = 9)
+// GLOBAL: NFSW 0x005134d8
+// int mirrorsignal;
 
 // GLOBAL - 0001:00046fe4 _mirrorthreadfunc 00447fe4 (static pubdef) (code)
+// LOCAL  - 0164: NEAR_RTN_386 (return type = 340) (none)
+// STUB: NFSW 0x00447fe4
+GAME_FUNC void __cdecl mirrorthreadfunc(void)
+{
+    // int byteoffset; // 018A: LOCAL (type = 9)
+    // int blockmask; // 0199: LOCAL (type = 9)
+    // int blockindex; // 01A7: LOCAL (type = 9)
+    // int s; // 01B6: LOCAL (type = 9)
+    // int filesize; // 01BC: LOCAL (type = 9)
+    // struct WIN32_FILE* file; // 01C9: LOCAL (type = 349)
+
+    // 0118: BLOCK_386 (parent = 0164) (addr = 004480AE)
+    // DWORD success; // 0124: LOCAL (type = 27)
+    // DWORD bytesread; // 0130: LOCAL (type = 27)
+    
+    // 013E: BLOCK_386 (parent = 0164) (addr = 004481A8)
+    // DWORD success; // 014A: LOCAL (type = 27)
+    // DWORD bytesread; // 0156: LOCAL (type = 27)
+
+    CALL_CDECL_VOID_0(0x00447fe4);
+}
+
 // GLOBAL - 0001:00047287 IsDirSep_ 00448287 (static pubdef) (code)
+// LOCAL  - 01D3: NEAR_RTN_386 (type = 350) (EAX)
+// ARG 0  - 01F4: LOCAL (type = 4)
+// STUB: NFSW 0x00448287
+GAME_FUNC int DEFAULT_CALL IsDirSep(char ch)
+{
+    CALL_WATCALL_RESULT_1(int, 0x00448287,
+        char ch,
+        ch
+    );
+}
+
 // GLOBAL - 0001:000472c6 wingetroot_ 004482c6 (static pubdef) (code)
+// LOCAL  - 01FB: NEAR_RTN_386 (return type = 351) (none)
+// ARG 0  - 021F: LOCAL (type = 13) (EAX)
+// ARG 1  - 0228: LOCAL (type = 13) (EDX)
+// STUB: NFSW 0x004482c6
+GAME_FUNC void DEFAULT_CALL wingetroot(char* path, char* root)
+{
+    // char* p; // 0231: LOCAL (type = 13)
+
+    CALL_WATCALL_VOID_2(0x004482c6,
+        char* path, char* root,
+        path, root
+    );
+}
+
 // GLOBAL - 0001:000473d0 winfullpath_ 004483d0 (static pubdef) (code)
+// LOCAL  - 0255: NEAR_RTN_386 (return type = 361) (EAX)
+// ARG 0  - 027D: LOCAL (type = 13) (EAX)
+// ARG 1  - 0288: LOCAL (type = 13) (EDX)
+// ARG 2  - 0294: LOCAL (type = 9) (EBX)
+// STUB: NFSW 0x004483d0
+GAME_FUNC int DEFAULT_CALL winfullpath(char* filein, char* pathout, int maxlen)
+{
+    // char* filepart; // 029F: LOCAL (type = 13)
+    // char root[4]; // 02AC: LOCAL (type = 362)
+    // DWORD dwFlags; // 02B7: LOCAL (type = 27)
+    // DWORD dwDummy; // 02C3: LOCAL (type = 27)
+
+    // 0237: BLOCK_386 (parent = 0255) (addr = 00448495)
+    // WIN32_FIND_DATA data; // 0243: LOCAL (type = 360)
+    // HANDLE h; // 024E: LOCAL (type = 108)
+
+    CALL_WATCALL_RESULT_3(int, 0x004483d0,
+        char* filein, char* pathout, int maxlen,
+        filein, pathout, maxlen
+    );
+}
+
 // GLOBAL - 0001:000474e3 _getdrivemap 004484e3 (code)
+// LOCAL  - 02CF: NEAR_RTN_386 (return type = 334) (EAX)
+// STUB: NFSW 0x004484e3
+GAME_FUNC int __cdecl getdrivemap(void)
+{
+    CALL_CDECL_RESULT_0(int, 0x004484e3);
+}
+
 // GLOBAL - 0001:00047503 _getdrivename 00448503 (code)
+// LOCAL  - 02F1: NEAR_RTN_386 (return type = 363) (EAX)
+// ARG 0  - 0314: LOCAL (type = 13)
+// ARG 1  - 031D: LOCAL (type = 13)
+// STUB: NFSW 0x00448503
+GAME_FUNC int __cdecl getdrivename(char* name, char* drivename)
+{
+    // char* temp; // 032B: LOCAL (type = 13)
+    // char* p; // 0334: LOCAL (type = 13)
+    // int map; // 033A: LOCAL (type = 9)
+    // int len; // 0342: LOCAL (type = 9)
+    // int di; // 034A: LOCAL (type = 9)
+
+    CALL_CDECL_RESULT_2(int, 0x00448503,
+        char* name, char* drivename,
+        name, drivename
+    );
+}
+
 // GLOBAL - 0001:00047616 _setdirectory 00448616 (code)
+// LOCAL  - 0351: NEAR_RTN_386 (return type = 364) (none)
+// ARG 0  - 0373: LOCAL (type = 13)
+// STUB: NFSW 0x00448616
+GAME_FUNC void __cdecl setdirectory(char* path)
+{
+    CALL_CDECL_VOID_1(0x00448616,
+        char* path,
+        path
+    );
+}
+
 // GLOBAL - 0001:0004765c _getdirectory 0044865c (code)
+// LOCAL  - 037C: NEAR_RTN_386 (return type = 364) (none)
+// ARG 0  - 039E: LOCAL (type = 13)
+// STUB: NFSW 0x0044865c
+GAME_FUNC void __cdecl getdirectory(char* path)
+{
+    // DWORD len; // 03A7: LOCAL (type = 27)
+
+    CALL_CDECL_VOID_1(0x0044865c,
+        char* path,
+        path
+    );
+}
+
 // GLOBAL - 0001:000476b7 _iswinfilesinited 004486b7 (code)
+// LOCAL  - 03AF: NEAR_RTN_386 (return type = 334) (EAX)
+// STUB: NFSW 0x004486b7
+GAME_FUNC int __cdecl iswinfilesinited(void)
+{
+    CALL_CDECL_RESULT_0(int, 0x004486b7);
+}
+
 // GLOBAL - 0001:000476ec _initwinfiles 004486ec (code)
+// LOCAL  - 03D6: NEAR_RTN_386 (return type = 365) (none)
+// ARG 0  - 03F8: LOCAL (type = 9)
+// STUB: NFSW 0x004486ec
+GAME_FUNC void __cdecl initwinfiles(int maxfiles)
+{
+    CALL_CDECL_VOID_1(0x004486ec,
+        int maxfiles,
+        maxfiles
+    );
+}
+
 // GLOBAL - 0001:00047791 _restorewinfiles 00448791 (code)
+// LOCAL  - 0417: NEAR_RTN_386 (return type = 340) (none)
+// STUB: NFSW 0x00448791
+GAME_FUNC void __cdecl restorewinfiles(void)
+{
+    // 0405: BLOCK_386 (parent = 0417) (addr = 004487AA)
+    // int i; // 0411: LOCAL (type = 9)
+
+    CALL_CDECL_VOID_0(0x00448791);
+}
+
 // GLOBAL - 0001:0004787c _openwinfile 0044887c (code)
+// LOCAL  - 0469: NEAR_RTN_386 (return type = 366) (EAX)
+// ARG 0  - 048B: LOCAL (type = 13)
+// ARG 1  - 0494: LOCAL (type = 9)
+// ARG 2  - 04A2: LOCAL (type = 9)
+// ARG 3  - 04B3: LOCAL (type = 41)
+// STUB: NFSW 0x0044887c
+GAME_FUNC int __cdecl openwinfile(char* name, int modeflags, int reqblocksize, int* handle)
+{
+    // int h; // 04BE: LOCAL (type = 9)
+    // int drive; // 04C4: LOCAL (type = 9)
+    // int success; // 04CE: LOCAL (type = 9)
+    // char drivename[4]; // 04DA: LOCAL (type = 367)
+    // DWORD accessmode; // 04EA: LOCAL (type = 27)
+    // DWORD sharemode; // 04F9: LOCAL (type = 27)
+    // DWORD createmode; // 0507: LOCAL (type = 27)
+    // DWORD attributes; // 0516: LOCAL (type = 27)
+    // DWORD sectors; // 0525: LOCAL (type = 27)
+    // DWORD bytes; // 0531: LOCAL (type = 27)
+    // DWORD freeblocks; // 053B: LOCAL (type = 27)
+    // DWORD totalblocks; // 054A: LOCAL (type = 27)
+
+    // 043C: BLOCK_386 (parent = 0469) (addr = 00448BD9)
+    // int protectflags; // 0448: LOCAL (type = 9)
+    // int viewflags; // 045A: LOCAL (type = 9)
+
+    CALL_CDECL_RESULT_4(int, 0x0044887c,
+        char* name, int modeflags, int reqblocksize, int* handle,
+        name, modeflags, reqblocksize, handle
+    );
+}
+
 // GLOBAL - 0001:00047f54 _infowinfile 00448f54 (code)
+// LOCAL  - 0575: NEAR_RTN_386 (return type = 369) (EAX)
+// ARG 0  - 0597: LOCAL (type = 9)
+// ARG 1  - 05A2: LOCAL (type = 41)
+// ARG 2  - 05B0: LOCAL (type = 41)
+// ARG 3  - 05BE: LOCAL (type = 41)
+// ARG 4  - 05CB: LOCAL (type = 41)
+// STUB: NFSW 0x00448f54
+GAME_FUNC int __cdecl infowinfile(int handle, int* modeflags, int* blocksize, int* filesize, int* freespace)
+{
+    // DWORD sectors; // 05D9: LOCAL (type = 27)
+    // DWORD bytes; // 05E5: LOCAL (type = 27)
+    // DWORD freeblocks; // 05EF: LOCAL (type = 27)
+    // DWORD totalblocks; // 05FE: LOCAL (type = 27)
+    // int drive; // 060E: LOCAL (type = 9)
+    // int h; // 0618: LOCAL (type = 9)
+    // int success; // 061E: LOCAL (type = 9)
+
+    // 055A: BLOCK_386 (parent = 0575) (addr = 0044902D)
+    // char drivename[4]; // 0566: LOCAL (type = 368)
+
+    CALL_CDECL_RESULT_5(int, 0x00448f54,
+        int handle, int* modeflags, int* blocksize, int* filesize, int* freespace,
+        handle, modeflags, blocksize, filesize, freespace
+    );
+}
+
 // GLOBAL - 0001:000480bd _readwinfile 004490bd (code)
+// LOCAL  - 0699: NEAR_RTN_386 (return type = 370) (EAX)
+// ARG 0  - 06BB: LOCAL (type = 9)
+// ARG 1  - 06C6: LOCAL (type = 13)
+// ARG 2  - 06D1: LOCAL (type = 9)
+// STUB: NFSW 0x004490bd
+GAME_FUNC int __cdecl readwinfile(int handle, char* buffer, int amount)
+{
+    // DWORD bytesread; // 06DC: LOCAL (type = 27)
+    // DWORD blksize; // 06EA: LOCAL (type = 27)
+    // DWORD totalbytes; // 06F6: LOCAL (type = 27)
+    // DWORD filesize; // 0705: LOCAL (type = 27)
+    // DWORD drive; // 0712: LOCAL (type = 27)
+    // int h; // 071C: LOCAL (type = 9)
+
+    // 0680: BLOCK_386 (parent = 0699) (addr = 00449227)
+    // int mirrored; // 068C: LOCAL (type = 9)
+
+    // 0667: BLOCK_386 (parent = 0680) (addr = 0044928F)
+    // int havedata; // 0673: LOCAL (type = 9)
+
+    // 062A: BLOCK_386 (parent = 0667) (addr = 004492AE)
+    // int blockmask; // 0636: LOCAL (type = 9)
+    // int blockindex; // 0644: LOCAL (type = 9)
+
+    // 0653: BLOCK_386 (parent = 0667) (addr = 004494FE)
+    // int err ; // 065F: LOCAL (type = 9)
+
+    CALL_CDECL_RESULT_3(int, 0x004490bd,
+        int handle, char* buffer, int amount,
+        handle, buffer, amount
+    );
+}
+
 // GLOBAL - 0001:0004858b _writewinfile 0044958b (code)
+// LOCAL  - 0736: NEAR_RTN_386 (return type = 370) (EAX)
+// ARG 0  - 0759: LOCAL (type = 9)
+// ARG 1  - 0764: LOCAL (type = 13)
+// ARG 2  - 076F: LOCAL (type = 9)
+// STUB: NFSW 0x0044958b
+GAME_FUNC int __cdecl writewinfile(int handle, char* buffer, int amount)
+{
+    // DWORD byteswritten; // 077A: LOCAL (type = 27)
+    // DWORD blksize; // 078B: LOCAL (type = 27)
+    // DWORD totalbytes; // 0797: LOCAL (type = 27)
+    // DWORD filesize; // 07A6: LOCAL (type = 27)
+    // int drive; // 07B3: LOCAL (type = 9)
+    // int h; // 07BD: LOCAL (type = 9)
+
+    // 0722: BLOCK_386 (parent = 0736) (addr = 004497CC)
+    // int err; // 072E: LOCAL (type = 9)
+
+    CALL_CDECL_RESULT_3(int, 0x0044958b,
+        int handle, char* buffer, int amount,
+        handle, buffer, amount
+    );
+}
+
 // GLOBAL - 0001:00048859 _seekwinfile 00449859 (code)
 // GLOBAL - 0001:00048a02 _tellwinfile 00449a02 (code)
 // GLOBAL - 0001:00048af2 _closewinfile 00449af2 (code)
--- gamesrc/nfs2/game/common/mpause.c
+++ gamesrc/nfs2/game/common/mpause.c
@@ -3,17 +3,88 @@
 #include <game/common/mpause.h>
 
 // GLOBAL - 0002:0000ccc8 _gUpPressTbl 004d9cc8 (data)
+// LOCAL  - 00AB: MODULE_386 (type = 65)
+// GLOBAL: NFSW 0x004d9cc8
+// char gUpPressTbl[2][2][5];
+
 // GLOBAL - 0002:0000ccdc _gDownPressTbl 004d9cdc (data)
+// LOCAL  - 00BF: MODULE_386 (type = 68)
+// GLOBAL: NFSW 0x004d9cdc
+// char gDownPressTbl [2][2][5];
 
 // GLOBAL - 0003:00007608 _gMPauseSliderVal 004f0608 (bss)
+// LOCAL  - 0000: MODULE_386 (type = 62)
+// GLOBAL: NFSW 0x004f0608
+// int gMPauseSliderVal[2];
+
 // GLOBAL - 0003:00007610 _gMPauseButton 004f0610 (bss)
+// LOCAL  - 0019: MODULE_386 (type = 9)
+// GLOBAL: NFSW 0x004f0610
+// int gMPauseButton;
+
 // GLOBAL - 0003:00007614 _gMPauseFile 004f0614 (bss)
+// LOCAL  - 002F: MODULE_386 (type = 13)
+// GLOBAL: NFSW 0x004f0614
+// char* gMPauseFile;
+
 // GLOBAL - 0003:00007618 _gMPauseMode 004f0618 (bss)
+// LOCAL  - 0043: MODULE_386 (type = 9)
+// GLOBAL: NFSW 0x004f0618
+// int gMPauseMode;
+
 // GLOBAL - 0003:0000761c _gMPauseRaceMusicGreyOut 004f061c (bss)
+// LOCAL  - 0057: MODULE_386 (type = 9)
+// GLOBAL: NFSW 0x004f061c
+// int gMPauseRaceMusicGreyOut;
+
 // GLOBAL - 0003:00007620 _gMPauseRestartGreyOut 004f0620 (bss)
+// LOCAL  - 0077: MODULE_386 (type = 9)
+// GLOBAL: NFSW 0x004f0620
+// int gMPauseRestartGreyOut;
+
 // GLOBAL - 0003:00007624 _gMPauseUpdate 004f0624 (bss)
+// LOCAL  - 0095: MODULE_386 (type = 9)
+// GLOBAL: NFSW 0x004f0624
+// int gMPauseUpdate;
 
 // GLOBAL - 0001:0002915e MPause_Logic_ 0042a15e (code)
+// LOCAL  - 0103: NEAR_RTN_386 (return type = 69) (EAX)
+// STUB: NFSW 0x0042a15e
+GAME_FUNC int DEFAULT_CALL MPause_Logic(void)
+{
+    // 00DD: BLOCK_386 (parent = 0103) (addr = 0042A2E2)
+    // int sv; // 00E9: LOCAL (type = 9)
+
+    // 00F0: BLOCK_386 (parent = 0103) (addr = 0042A3D4)
+    // int sv; // 00FC: LOCAL (type = 9)
+
+    CALL_WATCALL_RESULT_0(int, 0x0042a15e);
+}
+
 // GLOBAL - 0001:0002959b MPause_Render_ 0042a59b (code)
+// LOCAL  - 0125: NEAR_RTN_386 (return type = 21) (none)
+// STUB: NFSW 0x0042a59b
+GAME_FUNC void DEFAULT_CALL MPause_Render(void)
+{
+    CALL_WATCALL_VOID_0(0x0042a59b);
+}
+
 // GLOBAL - 0001:000295b5 MPause_InitMPause_ 0042a5b5 (code)
+// LOCAL  - 0147: NEAR_RTN_386 (return type = 21) (none)
+// STUB: NFSW 0x0042a5b5
+GAME_FUNC void DEFAULT_CALL MPause_InitMPause(void)
+{
+    // char name[255]; // 016D: LOCAL (type = 70)
+    // int language; // 0177: LOCAL (type = 9)
+    // char langname [6][16]; // 0184: LOCAL (type = 72)
+
+    CALL_WATCALL_VOID_0(0x0042a5b5);
+}
+
 // GLOBAL - 0001:000296f7 MPause_KillMPause_ 0042a6f7 (code)
+// LOCAL  - 0191: NEAR_RTN_386 (return type = 21) (none)
+// STUB: NFSW 0x0042a6f7
+GAME_FUNC void DEFAULT_CALL MPause_KillMPause(void)
+{
+    CALL_WATCALL_VOID_0(0x0042a6f7);
+}
